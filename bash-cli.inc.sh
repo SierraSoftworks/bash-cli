ac235d013368a96b218ed822fe097eecb0bab723
Andre Herbst <moormaster@gmx.net>
2021-10-01 22:27:49 +0200

    cli.sh: removed realpath() function and move contents into bcli_resolve_repath() function

--- bash-cli.inc.sh
+++ bash-cli.inc.sh
@@ -12,7 +12,8 @@ COLOR_LIGHT_GRAY="\033[37m"
 COLOR_DARK_GRAY="\033[38m"
 COLOR_NORMAL="\033[39m"
 
-realpath() {
+
+function bcli_resolve_path() {
     if [ -x "$( which realpath )" ]
     then
         # call the realpath utility if installed
@@ -22,10 +23,6 @@ realpath() {
         # -> use fallback to perl
         perl -e 'use Cwd "abs_path"; print abs_path(shift)' "$1"
     fi
-}
-
-function bcli_resolve_path() {
-    realpath "$1"
 }
 
 function bcli_trim_whitespace() {
